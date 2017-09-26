d15cbefeff7db1ef197073ad51187b46cbe63599
nikosChalk <chalkiadakisn@gmail.com>
2017-09-26 20:01:54 +0300

    Small optimization changes

--- neural_net.py
+++ neural_net.py
@@ -4,6 +4,7 @@ import numpy
 import random
 from tensorflow.examples.tutorials.mnist import input_data
 
+
 class AutoEncoder:
 
     mnist_dataset = input_data.read_data_sets("MNIST_data", one_hot=True)
@@ -11,11 +12,10 @@ class AutoEncoder:
 
     def __init__(self, layers):
         """
-        Creates an AutoEncoder with the given layers which uses the Stochastic Gradient Descent optimizer. Samples of this encoder
-         are assumed to be in range [0, 1]
-        :param layers: A list which contains the number of layers of each layer. This list is assumed to be symmetrical and with odd size
+        Creates an AutoEncoder with the given layers which uses the Adadelta optimizer. Samples of this encoder are assumed to be in range [0, 1]
+        :param layers: A list which contains the number of neurons of each layer. This list is assumed to be symmetrical and with odd size
         """
-        if(len(layers)%2 == 0): # Checking if size is odd
+        if(len(layers)%2 == 0): # Checking if list size is odd
             raise ValueError('Number of layers must be an odd number')
 
         first_half = layers[0: (int)(len(layers)/2)]
@@ -32,11 +32,11 @@ class AutoEncoder:
         self._encoder_bmatrix = []   # contains Tesnors which represent the bias vector for the encoder's layers. b[i] = bias vector for layer (i+1)
         self._decoder_bmatrix = []   # contains Tesnors which represent the bias vector for the decoder's layers. b[i] = bias vector for layer (i+1)
         self._sess = tf.Session()
-        self._summary_keys = ['per_batch', 'per_epoch', 'test_summary']
+        self._summary_keys = ['per_batch', 'per_epoch', 'test_summary'] #keys which are used to group summaries
 
         print('Initializing making of Computational Graph...')
         # Making the Computational Graph
-        # Define the initialization of the weights and the biases
+        # Define the initialization of the weights and the biases.
         for i in range(1, (int)(len(layers)/2)+1):
             self._encoder_wmatrix.append(tf.Variable(tf.truncated_normal([layers[i], layers[i-1]], stddev=0.0499), name=('encoder_wmatrix_' + str(i-1)) ))
             self._encoder_bmatrix.append(tf.Variable(tf.truncated_normal([layers[i], 1], stddev=0.0499), name=('encoder_bmatrix_' + str(i-1)) ))
@@ -53,17 +53,17 @@ class AutoEncoder:
             tf.summary.histogram(self._decoder_bmatrix[i].name, self._decoder_bmatrix[i], collections=[self._summary_keys[0]])
 
         # Defining NN's input place holder
-        self._nn_inp_holder = tf.placeholder(dtype=tf.float32, shape=[self._d_x, None], name='nn_input_data')  # [d_x, batch_size]
+        self._nn_inp_holder = tf.placeholder(dtype=tf.float32, shape=[self._d_x, None], name='nn_input_data')  # [d_x, z], where z can be anything.
 
         # Defining NN's Output
-        self._encoder_op = self._encode(self._nn_inp_holder)    # [2, batch_size]
-        self._y_hat = self._decode(self._encoder_op)            # [d_x, batch_zie]
+        self._encoder_op = self._encode(self._nn_inp_holder)    # [2, z]
+        self._y_hat = self._decode(self._encoder_op)            # [d_x, z]
 
         # Defining NN's cost function
         with tf.name_scope('cost', values=[self._nn_inp_holder, self._y_hat]):
-            self._cost = tf.reduce_sum(tf.square(tf.sub(self._nn_inp_holder, self._y_hat)), axis=1)  # [d_x, 1]
+            self._cost = tf.reduce_sum(tf.square(tf.sub(self._nn_inp_holder, self._y_hat)), axis=1)  # [d_x, 1]. Sum is over the samples
             self._cost = tf.mul(tf.constant(1/2, dtype=tf.float32), self._cost)
-            self._cost = tf.reduce_mean(self._cost, axis=0) # Scalar Value
+            self._cost = tf.reduce_mean(self._cost, axis=0) # Scalar Value. Sum is over the features
         tf.summary.scalar('batch_cost', self._cost, collections=[self._summary_keys[0]])
         tf.summary.scalar('test_cost', self._cost, collections=[self._summary_keys[2]])
 
@@ -85,15 +85,15 @@ class AutoEncoder:
         self._summaries_per_epoch = tf.summary.merge_all(key=self._summary_keys[1])
         self._test_summaries = tf.summary.merge_all(key=self._summary_keys[2])
 
-        print('Initialization of making Computational Graph completed!\n')
-        print('Initialization of Weight Variables...')
+        print('Initialization of Computational Graph completed!\n')
+        print('Initializing Variables...')
         self._sess.run(tf.global_variables_initializer())  #Initializes global variables and starts assessing the computation graph
-        print('Initializing of Weight Variables Done!\n')
+        print('Initialization of Variables Done!\n')
 
     def delete(self):
         """
-
-        :return:
+        Delete this Auto-encoder and release the Tensorflow resources that it has acquired.
+        :return: void
         """
         self._writer.close()
         self._sess.close()
@@ -101,7 +101,7 @@ class AutoEncoder:
 
     def _encode(self, data):
         """
-        Encodes the given sample
+        Encodes the given samples
         :param data: A Tensor of size [d_x, z], where z can be any number
         :return: A Tensor of size [d_y, z]
         """
@@ -116,7 +116,7 @@ class AutoEncoder:
 
     def _decode(self, data):
         """
-        Decodes the given sample
+        Decodes the given samples
         :param data: A Tensor of size [d_y, z], where z can be any number
         :return: A Tensor of size [d_x, z]
         """
@@ -131,7 +131,7 @@ class AutoEncoder:
 
     def train(self, total_epochs, batch_size):
         """
-        Trains the weights of the given Neural Network using the MNIST dataset
+        Trains the weights and the biases of the Neural Network using the MNIST dataset
         :param total_epochs: The epochs that the NN should run. Must be >0
         :param batch_size: The size of each batch. Must be >0 and must have 0 modulo with 55000
         :return void
@@ -148,20 +148,21 @@ class AutoEncoder:
         print('Total Batches per epoch are: ', total_batches)
 
         for cur_epoch in range(0, total_epochs):
-            batch_cost_list = []
+            batch_cost_list = []    # List which holds the cost (scalar value) for each batch for one epoch.
             for cur_batch in range(total_batches):
                 batch_x, _ = AutoEncoder.mnist_dataset.train.next_batch(batch_size)
-                batch_x = numpy.transpose(batch_x)
+                batch_x = numpy.transpose(batch_x)  #result is [d_x, batch_size]
                 c, _ = self._sess.run([self._cost, self._minimize_op], feed_dict={self._nn_inp_holder: batch_x})
                 batch_cost_list.append(c)
 
-                if((total_batches < 20) or (cur_batch % 15 == 0)): #Do not record all the batches. Just a few of them
+                # Do not record the results of all the batches. Just a few of them.
+                if((total_batches < 20) or (cur_batch % 15 == 0)):
                     cur_step = cur_epoch*total_batches + cur_batch
                     self._writer.add_summary(self._summaries_per_batch.eval(session=self._sess, feed_dict={self._nn_inp_holder: batch_x}), cur_step)
 
             # Defining which epochs should be recorded so that we do not have an overflow of metric data
             if( (total_epochs < 100) or (cur_epoch % 10 == 0)):
-                # Define image summaries for 4 random samples. (Must be done per iteration, since this summary has a buggy implementation
+                # Define image summaries for 4 random samples. (Must be done per iteration, since this summary has a buggy implementation)
                 input_img_summary = tf.summary.image('input_images_' + str(cur_epoch), tf.reshape(tf.transpose(self._nn_inp_holder), [-1, 28, 28, 1]), max_outputs=4)  # Get 4 images per epoch as a sample
                 output_img_summary = tf.summary.image('output_images_' + str(cur_epoch), tf.reshape(tf.transpose(self._y_hat), [-1, 28, 28, 1]), max_outputs=4)  # Get 4 images per epoch as a sample
                 img_summaries = tf.summary.merge([input_img_summary, output_img_summary])
@@ -181,7 +182,8 @@ class AutoEncoder:
 
     def test(self):
         """
-        Uses the test dataset from the MNIST for testing. Note that train() must have been called beforehand.
+        Uses the test dataset from the MNIST for testing. Note that train() must have been called beforehand, otherwise
+        the behaviour is undefined.
         :return: The cost of the dataset
         """
         print('Initializing Testing of NN...')
@@ -191,11 +193,11 @@ class AutoEncoder:
         test_labels = numpy.transpose(AutoEncoder.mnist_dataset.test.labels)
         sample_images = numpy.empty(shape=(self._d_x,0), dtype=float)
         sample_labels = [0] * AutoEncoder.num_of_classes()
-        i = random.randint(0, AutoEncoder.test_samples()-1)
+        i = random.randint(0, AutoEncoder.test_samples()-1) #random index to start the slice.
         samples_taken = 0
-        while(samples_taken < AutoEncoder.num_of_classes()):
-            cur_img = numpy.argmax(test_labels[:, i])
-            if(sample_labels[cur_img] == 0):
+        while(samples_taken < AutoEncoder.num_of_classes()):    #Take 10 different samples. Start searching from index i.
+            cur_img = numpy.argmax(test_labels[:, i])   #value in range [0, 9]
+            if(sample_labels[cur_img] == 0):    #Sample has not been taken.
                 sample_images = numpy.concatenate((sample_images, numpy.reshape(test_images[:, i], (-1, 1))), axis=1)
                 sample_labels[cur_img] = 1
                 samples_taken += 1
@@ -217,42 +219,39 @@ class AutoEncoder:
 
     def _fc_layer(self, weight_matrix, layer_input, bias_matrix, op_name='fc_layer'):
         """
-        Calculates the output of a fully connected layer using the tanh() activation function
+        Calculates the output of a fully connected layer using the relu() activation function
         :param weight_matrix: A Tensor which holds the values by which the input is multiplied
         :param layer_input: A Tensor which is the input for the layer
         :param bias_matrix: A Tensor which holds the biases
         :param op_name: A name for the output of this operation
-        :return: The output of this layer, which is tanh(weight_matrix * input)
+        :return: The output of this layer, which is relu((weight_matrix * input) + bias_matrix)
         """
-        with tf.name_scope(op_name, values=[weight_matrix, layer_input]):
+        with tf.name_scope(op_name, values=[weight_matrix, bias_matrix, layer_input]):
             output = tf.nn.relu(tf.add(tf.matmul(weight_matrix, layer_input), bias_matrix)) # Broadcasting is used for performing the add operation
             tf.summary.histogram(op_name, output, collections=[self._summary_keys[0]])
             return output
 
     def _enc_output_layer(self, weight_matrix, layer_input, bias_matrix, op_name='encoder_output_layer'):
         """
-        Calculates the output of the encoder, which is a fully connected layer with no activation function.
+        Calculates the output of the encoder, which is a fully connected layer with relu(x) activation function.
         :param weight_matrix: A Tensor which holds the values by which the input is multiplied
         :param layer_input: A Tensor which is the input for the layer
         :param bias_matrix: A Tensor which holds the biases
         :param op_name: A name for the output of this operation
-        :return: The output of this layer, (weight_matrix * input)
+        :return: The output of this layer which is relu((weight_matrix * input) + bias_matrix)
         """
-        with tf.name_scope(op_name, values=[weight_matrix, layer_input]):
-            output = tf.nn.relu(tf.add(tf.matmul(weight_matrix, layer_input), bias_matrix))     # Broadcasting is used for performing the add operation
-            tf.summary.histogram(op_name, output, collections=[self._summary_keys[0]])
-            return output
+        return self._fc_layer(weight_matrix, layer_input, bias_matrix, op_name)
 
     def _dec_output_layer(self, weight_matrix, layer_input, bias_matrix, op_name='decoder_output_layer'):
         """
-        Calculates the output of the decoder, which is a fully connected layer with softmax activation function.
+        Calculates the output of the decoder, which is a fully connected layer with sigmoid(x) activation function.
         :param weight_matrix: A Tensor which holds the values by which the input is multiplied
         :param layer_input: A Tensor which is the input for the layer
         :param bias_matrix: A Tensor which holds the biases
         :param op_name: A name for the output of this operation
-        :return: The output of this layer which is softmax(weight_matrix * input)
+        :return: The output of this layer which is sigmoid((weight_matrix * input) + bias_matrix)
         """
-        with tf.name_scope(op_name, values=[weight_matrix, layer_input]):
+        with tf.name_scope(op_name, values=[weight_matrix, bias_matrix, layer_input]):
             output = tf.nn.sigmoid(tf.add(tf.matmul(weight_matrix, layer_input), bias_matrix))  # Broadcasting is used for performing the add operation
             tf.summary.histogram(op_name, output, collections=[self._summary_keys[0]])
             return output
